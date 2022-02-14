b8e9dc83dee325f0ecd4016148c519a1aee0134a
Cédric Rodony <cedric.rodony@gmail.com>
2022-02-14 23:41:43 +0100

    Version avec les images

--- articles/liste_articles.php
+++ articles/liste_articles.php
@@ -7,12 +7,14 @@ require_once "Articles.php";
 $entry = Articles::all();
 
 foreach($entry as $unarticle){
+    $img=$unarticle->img;
     echo "Ref. : ".$unarticle->ref."<br/><br/>";
     echo "<a href='article.php?id=".$unarticle->id."'>$unarticle->titre</a><br/><br/>";
     echo "$unarticle->description<br/><br/>";
     echo "$unarticle->prix"." €<br/><br/>";
+    echo "<img class=photosListe src='$img'/>"."<br/><br/>";
+    // echo "<img src=".$unarticle->img."/>"."<br/><br/>";
     echo "<button><a href='modification.php?id=".$unarticle->id."&mode=modif&titre=".$unarticle->titre."'>Modifier </a></button> - <button><a href='suppression.php?id=".$unarticle->id."'> Supprimer </a></button><hr>";
-
     //<a href='unutilisateur.php?id=".$unarticle->id."'>$unarticle->prenom."  ".$unarticle->nom."</a> <hr>";
 }
 
