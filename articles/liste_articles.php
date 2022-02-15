956d2a08d5a63c261fa003f6f4ee17a7e0b05776
Cédric Rodony <cedric.rodony@gmail.com>
2022-02-15 12:49:05 +0100

    Version avec une autre disposition des aarticles

--- articles/liste_articles.php
+++ articles/liste_articles.php
@@ -1,23 +1,25 @@
 <?php
-// Connecter à la BD
 require "startbd.php";
 bd();
 require_once "Articles.php";
-// recuperer l ensemble des données des utilisateurs
 $entry = Articles::all();
-
-foreach($entry as $unarticle){
-    $img=$unarticle->img;
-    echo "Ref. : ".$unarticle->ref."<br/><br/>";
-    echo "<a href='article.php?id=".$unarticle->id."'>$unarticle->titre</a><br/><br/>";
-    echo "$unarticle->description<br/><br/>";
-    echo "$unarticle->prix"." €<br/><br/>";
-    echo "<img class=photosListe src='$img'/>"."<br/><br/>";
-    // echo "<img src=".$unarticle->img."/>"."<br/><br/>";
-    echo "<button><a href='modification.php?id=".$unarticle->id."&mode=modif&titre=".$unarticle->titre."'>Modifier </a></button> - <button><a href='suppression.php?id=".$unarticle->id."'> Supprimer </a></button><hr>";
-    //<a href='unutilisateur.php?id=".$unarticle->id."'>$unarticle->prenom."  ".$unarticle->nom."</a> <hr>";
-}
-
-
-// Recuperer l'ensemble des donnes de la tables utilisateurs
 ?>
+
+<section id="listeArticles">
+
+<?php
+foreach($entry as $unarticle){
+    $img=$unarticle->img; ?>
+    <article class="produit">
+        <p class="ref">Ref. : <?php echo "$unarticle->ref"; ?></p>
+        <p class="titre"><a href="article.php?id=<?php echo "$unarticle->id";?>"><?php echo "$unarticle->titre";?></a></p>
+        <p class="description"><?php echo "$unarticle->description";?>
+        <p class="prix"><span style="text-decoration: underline;">Prix :</span> <?php echo "$unarticle->prix €";?></p>
+        <p class="photos"><img src="<?php echo "$img";?>"></p>
+        <p class="modsup"><button><a href="modification.php?id=<?php echo $unarticle->id."&mode=modif&titre=".$unarticle->titre?>">Modifier</a></button><button><a href="suppression.php?id=<?php echo $unarticle->id;?>"> Supprimer </a></button></p>
+    </article>
+<?php
+}
+?>
+
+</section>
